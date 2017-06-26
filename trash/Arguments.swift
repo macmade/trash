98fd6d491e18aa88277e4627497e274a76fdf66c
macmade <macmade@xs-labs.com>
2017-06-26 23:31:59 +0200

    Initial project import...

---
+++ trash/Arguments.swift
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        Arguments.swift
+ * @copyright   (c) 2017, Jean-David Gadina - www.xs-labs.com
+ */
+
+import Foundation
+
+class Arguments
+{
+    public private( set ) var directories = false
+    public private( set ) var force       = false
+    public private( set ) var interactive = false
+    public private( set ) var recursive   = false
+    public private( set ) var verbose     = false
+    public private( set ) var files       = [ String ]()
+    
+    public init( _ arguments: [ String ] ) throws
+    {
+        if( arguments.count < 2 )
+        {
+            throw Error.RuntimeError( self.usage() ?? "Unknown error" )
+        }
+        
+        for arg in arguments.dropFirst()
+        {
+            if( arg.hasPrefix( "-" ) && self.files.count > 0 )
+            {
+                throw Error.RuntimeError( self.usage() ?? "Invalid arguments" )
+            }
+            else if( arg.hasPrefix( "-" ) )
+            {
+                let index = arg.index( arg.startIndex, offsetBy: 1 )
+                let args  = arg.substring( from: index ).characters
+                
+                for c in args
+                {
+                    if( c == "d" )
+                    {
+                        self.directories = true
+                    }
+                    else if( c == "f" )
+                    {
+                        self.interactive = false
+                        self.force       = true
+                    }
+                    else if( c == "i" )
+                    {
+                        self.interactive = true
+                        self.force       = false
+                    }
+                    else if( c == "R" || c == "r" )
+                    {
+                        self.recursive = true
+                    }
+                    else if( c == "v" )
+                    {
+                        self.verbose = true
+                    }
+                    else
+                    {
+                        throw Error.RuntimeError( "Unrecognized argument \( arg )" )
+                    }
+                }
+            }
+            else
+            {
+                self.files.append( arg )
+            }
+        }
+    }
+    
+    public func usage() -> String?
+    {
+        let task   = Process()
+        let stdout = Pipe()
+        
+        task.launchPath     = "/bin/rm"
+        task.arguments      = []
+        task.standardOutput = stdout
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        let data = stdout.fileHandleForReading.readDataToEndOfFile()
+        
+        return String( data: data, encoding: .utf8 )?.trimmingCharacters( in: CharacterSet.whitespacesAndNewlines )
+    }
+    
+    public enum Error: Swift.Error
+    {
+        case RuntimeError( String )
+    }
+}
---
+++ trash/main.swift
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        main.swift
+ * @copyright   (c) 2017, Jean-David Gadina - www.xs-labs.com
+ */
+
+import Cocoa
+
+let current = NSURL.fileURL( withPath: "." )
+let parent  = NSURL.fileURL( withPath: ".." )
+
+do
+{
+    let args = try Arguments( ProcessInfo.processInfo.arguments )
+    
+    if( args.files.count == 0 )
+    {
+        print( args.usage() ?? "No files provided" )
+        exit( -1 )
+    }
+    
+    for file in args.files
+    {
+        var isDir: ObjCBool = false
+        
+        if( file.utf8.count == 0 ) 
+        {
+            print( "rm: empty file path" )
+            exit( -1 )
+        }
+        
+        let path = ( NSString( string: file ).expandingTildeInPath as NSString ).standardizingPath as String
+        
+        if( path.utf8.count == 0 )
+        {
+            print( "rm: empty file path" )
+            exit( -1 )
+        }
+        
+        let exists = FileManager.default.fileExists( atPath: path, isDirectory: &isDir )
+        let url    = NSURL( fileURLWithPath: path, isDirectory: isDir.boolValue ) as URL
+        
+        if( exists == false )
+        {
+            if( args.force == false )
+            {
+                print( "rm: \( file ): No such file or directory" )
+                exit( -1 )
+            }
+            
+            continue;
+        }
+        
+        if( url.path == current.path || url.path == parent.path )
+        {
+            print( "rm: \".\" and \"..\" may not be removed" )
+            exit( -1 )
+        }
+        
+        if( isDir.boolValue )
+        {
+            if( args.directories == false )
+            {
+                print( "rm: \( file ): is a directory" )
+                exit( -1 )
+            }
+            
+            do
+            {
+                let isEmpty = try FileManager.default.contentsOfDirectory( at: url, includingPropertiesForKeys: [], options: .skipsSubdirectoryDescendants ).count == 0
+                
+                if( isEmpty == false && args.recursive == false )
+                {
+                    print( "rm: \( file ): Directory not empty" )
+                    exit( -1 )
+                }
+            }
+            catch
+            {
+                print( "rm: \( file ): Cannot list directory" )
+                exit( -1 )
+            }
+        }
+        
+        if( args.interactive )
+        {
+            print( "remove \( file )? ", terminator: "" )
+            
+            let input = readLine() ?? "n"
+            
+            if( input.trimmingCharacters( in: CharacterSet.whitespacesAndNewlines).lowercased() != "y" )
+            {
+                exit( -1 )
+            }
+        }
+        
+        do
+        {
+            try FileManager.default.trashItem( at: url, resultingItemURL: nil )
+        }
+        catch
+        {
+            print( "rm: \( file ): Cannot move to trash" )
+            exit( -1 )
+        }
+    }
+}
+catch Arguments.Error.RuntimeError( let message )
+{
+    print( message )
+    exit( -1 )
+}
